f95ebcf81fef743a0a908dd2248a39728096f7a1
Rustam Kasad <rustamkasad@gmail.com>
2019-08-30 23:07:10 -0700

    Import events. Fix up date link.

--- src/components/VenuePage.js
+++ src/components/VenuePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import DatePickerLink from './DatePickerLink';
 import moment from 'moment';
+import shows from '../events.js';
 
 const ShowContainer = styled.div`
   margin-top: 1rem;
@@ -15,16 +16,25 @@ const ShowContainer = styled.div`
 `;
 
 const VenuePage = ({ venueName }) => {
+  const dateFormat = "YYYY-MM-DD";
   return (
     <div>
-      VENUE PAGE {venueName}
+      <h3>VENUE: {venueName}</h3>
       {shows
       .filter(s => s.venueName === venueName)
       .map(show => 
         <ShowContainer>
-          <DatePickerLink date={moment()} />
+          <DatePickerLink 
+            date={moment(show.utcDate)} 
+            uri={`/shows/${moment(show.utcDate).format(dateFormat)}`}
+          />
           <div>
-            {show.bands.map(band => <span>{band} </span>)}
+            <div>
+              <em>{show.headliner}</em>
+            </div>
+            <div>
+              {show.support.map(band => <span>{band} - </span>)}
+            </div>
           </div>
         </ShowContainer>
       )}
@@ -32,47 +42,47 @@ const VenuePage = ({ venueName }) => {
   );
 };
 
-const shows = [{
-    id: 1,
-    venueName:"Casbah",
-    bands:['Heavy Celebration', 'Nice Guys', 'Bedroom Slippers']
-  }, {
-    id: 2,
-    venueName:"Sodabar", 
-    bands:['Creepies', 'Jerks', 'Silent Rockers']
-  }, {
-    id: 3,
-    venueName:"TilTwo Club",
-    bands:['Jimmy and the Jimmys', 'Quiet Muse', 'Silent Be-Bop']
-  }, {
-    id: 4,
-    venueName:"Sodabar", 
-    bands:['Creepies', 'Jerks', 'Silent Rockers']
-  }, {
-    id: 5,
-    venueName:"Music Box",
-    bands:['Mongos', 'Sleepies', 'Loud List Orchestra']
-  }, {
-    id: 6,
-    venueName:"Black Cat Bar",
-    bands:['Catsy Cats', 'Dogsy Dogs', 'Loud Rockers']
-  }, {
-    id: 7,
-    venueName:"Bancroft Bar",
-    bands:['Sillies', 'Serious Folk', 'Dirty Clowns']
-  }, {
-    id: 8,
-    venueName:"Funny Farm",
-    bands:['Sillies', 'Morose Folk', 'Crazy Clowns']
-  }, {
-    id: 9,
-    venueName:"Non-alcoholic Bar",
-    bands:['Teetotalers', 'Serious Folk', 'Sober Clowns']
-  }, {
-    id: 10,
-    venueName:"Alcohol Bar",
-    bands:['Libertines', 'Funny People', 'Drunken Clowns']
-  }
-];
+// const shows = [{
+//     id: 1,
+//     venueName:"Casbah",
+//     bands:['Heavy Celebration', 'Nice Guys', 'Bedroom Slippers']
+//   }, {
+//     id: 2,
+//     venueName:"Sodabar", 
+//     bands:['Creepies', 'Jerks', 'Silent Rockers']
+//   }, {
+//     id: 3,
+//     venueName:"TilTwo Club",
+//     bands:['Jimmy and the Jimmys', 'Quiet Muse', 'Silent Be-Bop']
+//   }, {
+//     id: 4,
+//     venueName:"Sodabar", 
+//     bands:['Creepies', 'Jerks', 'Silent Rockers']
+//   }, {
+//     id: 5,
+//     venueName:"Music Box",
+//     bands:['Mongos', 'Sleepies', 'Loud List Orchestra']
+//   }, {
+//     id: 6,
+//     venueName:"Black Cat Bar",
+//     bands:['Catsy Cats', 'Dogsy Dogs', 'Loud Rockers']
+//   }, {
+//     id: 7,
+//     venueName:"Bancroft Bar",
+//     bands:['Sillies', 'Serious Folk', 'Dirty Clowns']
+//   }, {
+//     id: 8,
+//     venueName:"Funny Farm",
+//     bands:['Sillies', 'Morose Folk', 'Crazy Clowns']
+//   }, {
+//     id: 9,
+//     venueName:"Non-alcoholic Bar",
+//     bands:['Teetotalers', 'Serious Folk', 'Sober Clowns']
+//   }, {
+//     id: 10,
+//     venueName:"Alcohol Bar",
+//     bands:['Libertines', 'Funny People', 'Drunken Clowns']
+//   }
+// ];
 
 export default VenuePage;
